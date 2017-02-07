e4aa88dbfce1f00eb7489deb269c37c196575505
Steve Geluso <stevegeluso@gmail.com>
2017-02-07 15:26:53 -0800

    adding readme

---
+++ bank.py
@@ -0,0 +1,34 @@
+class BankAccount:
+  pass
+
+class ChildrensAccount:
+  pass
+
+class OverdraftAccount:
+  pass
+
+basic_account = BankAccount()
+basic_account.deposit(600)
+print("Basic account has ${}".format(basic_account.balance))
+basic_account.withdraw(17)
+print("Basic account has ${}".format(basic_account.balance))
+basic_account.accumulate_interest()
+print("Basic account has ${}".format(basic_account.balance))
+print()
+
+childs_account = ChildrensAccount()
+childs_account.deposit(34)
+print("Child's account has ${}".format(childs_account.balance))
+childs_account.withdraw(17)
+print("Child's account has ${}".format(childs_account.balance))
+childs_account.accumulate_interest()
+print("Child's account has ${}".format(childs_account.balance))
+print()
+
+overdraft_account = OverdraftAccount()
+overdraft_account.deposit(12)
+print("Overdraft account has ${}".format(overdraft_account.balance))
+overdraft_account.withdraw(17)
+print("Overdraft account has ${}".format(overdraft_account.balance))
+overdraft_account.accumulate_interest()
+print("Overdraft account has ${}".format(overdraft_account.balance))
