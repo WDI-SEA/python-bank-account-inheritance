7d1d3b572b862b167eda64bd3d79671181d957b8
chidrome <chidrome@gmail.com>
2019-02-14 18:17:06 -0800

    finished homework

--- bank.py
+++ bank.py
@@ -1,11 +1,49 @@
 class BankAccount:
-  pass
+  def __init__(self, balance=0):
+    self.balance = balance
+    print('Your balance is: $', balance)
 
-class ChildrensAccount:
-  pass
+  def deposit(self, amount):
+    if amount < 0:
+      return False
+    else:
+      self.balance = self.balance + amount
+    # print('You have deposited: $', amount, '. Your balance is now: $', self.balance)
 
-class OverdraftAccount:
-  pass
+  def withdraw(self, amount):
+    if amount < 0:
+      return False
+    else:
+      self.balance = self.balance - amount
+    # print('You have withdrawn: $', amount, '. Your balance is now: $', self.balance)
+
+  def accumulate_interest(self):
+    self.balance = self.balance * 1.02
+
+class ChildrensAccount(BankAccount):
+  def __init__(self, balance=0):
+    self.balance = balance
+
+  def accumulate_interest(self):
+    self.balance += 10
+
+class OverdraftAccount(BankAccount):
+  def __init__(self, overdraft_penalty = 40, balance = 0):
+    self.balance = balance
+    self.overdraft_penalty = overdraft_penalty
+
+  def withdraw(self, amount):
+    if self.balance - amount < 0:
+      self.balance -= self.overdraft_penalty
+    else:
+      self.balance = self.balance - amount
+    
+  def accumulate_interest(self):
+    if self.balance < 1:
+      return self.balance
+    else:
+      self.balance = self.balance * 1.02
+
 
 basic_account = BankAccount()
 basic_account.deposit(600)
@@ -14,7 +52,7 @@ basic_account.withdraw(17)
 print("Basic account has ${}".format(basic_account.balance))
 basic_account.accumulate_interest()
 print("Basic account has ${}".format(basic_account.balance))
-print()
+# print()
 
 childs_account = ChildrensAccount()
 childs_account.deposit(34)
@@ -23,7 +61,7 @@ childs_account.withdraw(17)
 print("Child's account has ${}".format(childs_account.balance))
 childs_account.accumulate_interest()
 print("Child's account has ${}".format(childs_account.balance))
-print()
+# print()
 
 overdraft_account = OverdraftAccount()
 overdraft_account.deposit(12)
