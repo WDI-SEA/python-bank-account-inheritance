bf988b9b5160e07e8b7e1e58fde73cb0285fe78c
mtom92 <torresmoranm@gmail.com>
2019-05-23 08:24:16 -0700

    done

--- bank.py
+++ bank.py
@@ -1,11 +1,38 @@
-class BankAccount:
-  pass
+class BankAccount():
+  def __init__(self, balance=0):
+    self.balance = balance
+  def deposit(self, amount):
+    self.balance = self.balance + amount
+    return self.balance
+  def withdraw(self, amount):
+    self.balance = self.balance - amount
+    return self.balance
+  def accumulate_interest(self):
+    self.balance = self.balance + self.balance * .02
+    return self.balance
 
-class ChildrensAccount:
-  pass
+class ChildrensAccount(BankAccount):
+  def __init__(self, balance=0):
+    BankAccount.__init__(self)
+  def accumulate_interest(self):
+    self.balance += 10
+    return self.balance
 
-class OverdraftAccount:
-  pass
+
+class OverdraftAccount(BankAccount):
+  def __init__(self, overdraft_penalty = 40):
+    self.overdraft_penalty = overdraft_penalty
+    BankAccount.__init__(self)
+  def withdraw(self, amount):
+    if amount > self.balance:
+      self.balance -= (self.overdraft_penalty)
+      return self.balance
+  def accumulate_interest(self):
+    if self.balance <= 0:
+      return self.balance
+    else:
+      self.balance = self.balance + self.balance * .02
+      return self.balance
 
 basic_account = BankAccount()
 basic_account.deposit(600)
@@ -24,11 +51,3 @@ print("Child's account has ${}".format(childs_account.balance))
 childs_account.accumulate_interest()
 print("Child's account has ${}".format(childs_account.balance))
 print()
-
-overdraft_account = OverdraftAccount()
-overdraft_account.deposit(12)
-print("Overdraft account has ${}".format(overdraft_account.balance))
-overdraft_account.withdraw(17)
-print("Overdraft account has ${}".format(overdraft_account.balance))
-overdraft_account.accumulate_interest()
-print("Overdraft account has ${}".format(overdraft_account.balance))
