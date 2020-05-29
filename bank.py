20fc711609abad1e89d66a916c92bc33bb1895c4
Jummay77 <hansangho@yahoo.com>
2020-05-28 23:21:50 -0700

    hw done

--- bank.py
+++ bank.py
@@ -1,11 +1,51 @@
 class BankAccount:
-  pass
+  interest = .02
+  def __init__(self, balance = 0):
+    self.balance = balance
 
-class ChildrensAccount:
-  pass
+  def deposit(self, amount):
+    if amount < 0:
+      #make the the transaction for the amount
+      return False
+    else:
+        self.balance = self.balance + amount
+        return self.balance 
 
-class OverdraftAccount:
-  pass
+  def withdraw(self, amount):
+    if amount > self.balance or amount < 0:
+      return False
+    else:
+        self.balance = self.balance - amount
+        return self.balance 
+
+  def accumulate_interest(self):
+    self.balance = self.balance + self.balance * BankAccount.interest
+    return self.balance
+    # Probably will be using the int() method
+    # balance + balance*.02
+
+class ChildrensAccount(BankAccount):
+  interest = 0
+  def accumulate_interest(self):
+    self.balance = self.balance + 10
+    return self.balance  
+
+class OverdraftAccount(BankAccount):
+  def __init__(self, balance = 0, overdraft_penalty = 40):
+    self.balance = balance
+    self.overdraft_penalty = overdraft_penalty
+
+  def withdraw(self, amount):
+    if amount > self.balance:
+      self.balance = self.balance - self.overdraft_penalty
+      return False
+      
+  def accumulate_interest(self):
+    if self.balance < 0:
+      return self.balance
+    else:
+      self.balance = self.balance + self.balance * BankAccount.interest
+      return self.balance
 
 basic_account = BankAccount()
 basic_account.deposit(600)
