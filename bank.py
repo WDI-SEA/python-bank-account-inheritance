2f920dab8b114b9ea92c9407420b4752fa04b630
Blake DeGraw <bbdegraw@gmail.com>
2021-03-02 21:21:41 -0800

    all done

--- bank.py
+++ bank.py
@@ -46,3 +46,29 @@ class ChildrensAccount(BankAccount):
 class OverdraftAccount(BankAccount):
   def __init__(self, balance=0, interest_rate=0.02, overdraft_penalty=40):
     super().__init__(balance, interest_rate, overdraft_penalty)
+
+basic_account = BankAccount()
+basic_account.deposit(600)
+print("Basic account has ${}".format(basic_account.balance))
+basic_account.withdraw(17)
+print("Basic account has ${}".format(basic_account.balance))
+basic_account.accumulate_interest()
+print("Basic account has ${}".format(basic_account.balance))
+print()
+
+childs_account = ChildrensAccount()
+childs_account.deposit(34)
+print("Child's account has ${}".format(childs_account.balance))
+childs_account.withdraw(17)
+print("Child's account has ${}".format(childs_account.balance))
+childs_account.accumulate_interest()
+print("Child's account has ${}".format(childs_account.balance))
+print()
+
+overdraft_account = OverdraftAccount()
+overdraft_account.deposit(12)
+print("Overdraft account has ${}".format(overdraft_account.balance))
+overdraft_account.withdraw(17)
+print("Overdraft account has ${}".format(overdraft_account.balance))
+overdraft_account.accumulate_interest()
+print("Overdraft account has ${}".format(overdraft_account.balance))
