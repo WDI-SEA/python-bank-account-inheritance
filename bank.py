35067c500f0e2887113185773eeb2e3e59036c70
Brianhin94 <brianhin64@gmail.com>
2021-03-03 10:04:56 -0700

    done hw

--- bank.py
+++ bank.py
@@ -1,12 +1,69 @@
-class BankAccount:
-  pass
+class BankAccount():
+  def __init__(self):
+    self.balance = 0 ## change to a random?
+    self.interest_rate = .02
+  
+  def get_balance(self):
+    print(f'You account balance is {self.balance}')
 
-class ChildrensAccount:
-  pass
+  def withdraw(self, amount):
+    if (amount < 0):
+      print('False - Invalid Withdrawl amount, please try again.')
+      return False
+    else:
+      self.balance -= amount
+      print(f'You withdrew ${amount} and your balance is {self.balance}')
+  
+  def deposit(self, amount):
+    if (amount < 0):
+      print('False - Invalid Deposit amount, please try again.')
+      return False
+    else:
+      self.balance += amount
+      print(f"You deoposited ${amount} your remaining balance is {self.balance}")
 
-class OverdraftAccount:
-  pass
+  def accumulate_interest(self):
+    old_balance = self.balance
+    self.balance = self.balance + self.balance*self.interest_rate
+    print(f"You've accumulated interest and your new account balance is {self.balance} up from {old_balance}")
 
+## - interest rate of zero
+
+class ChildrensAccount(BankAccount):
+  def __init__(self):
+    super().__init__()
+    self.interest_rate = 0
+  
+  def accumulate_interest(self):
+    self.balance += 10
+    print(f"Congratulations! Your account gained a bonus interest and is now {self.balance}")
+
+
+## - subtract only overdraft_penalty
+class OverdraftAccount(BankAccount):
+  def __init__(self):
+    super().__init__()
+    self.overdraft_penalty = 40
+
+  def withdraw(self, amount):
+    if (amount > self.balance):
+      self.balance -= self.overdraft_penalty
+      print(f'You cannot withdraw that amount you have been deducted a fee of {self.overdraft_penalty} and your balance is now {self.balance}')
+    else:
+      self.balance -= amount
+      print(f'Your withdrew {amount} and your balance is now {self.balance}')
+
+  def accumulate_interest(self):
+    if (self.balance < 0):
+      print('Your acount is negative and you cannot accumulate any interest, deposit more money in order to!')
+      return
+    else: 
+      self.balance = self.balance + self.balance*self.interest_rate
+      print(f'Your accumulated interest and your balance is now {self.balance}')
+
+
+
+#prints
 basic_account = BankAccount()
 basic_account.deposit(600)
 print("Basic account has ${}".format(basic_account.balance))
