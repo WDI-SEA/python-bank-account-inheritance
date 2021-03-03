4c76475e2c092ee73c06febc0bda76a960ffc6f5
Your Name <you@example.com>
2021-03-02 21:27:42 -0800

    Completed assignment.

--- bank.py
+++ bank.py
@@ -1,11 +1,56 @@
 class BankAccount:
-  pass
+  # 'pass' is a line that says to do nothing; it's a placeholder value.
+  def __init__(self, interest_rate=0.02):
+    self.balance = 0
+    self.interest_rate = interest_rate
 
-class ChildrensAccount:
-  pass
+  def deposit(self, amount):
+    if amount < 0:
+      print('You cannot withdraw or deposit negative amounts.')
+      return False
+    self.balance += amount
+    print(f'After depositing ${amount}, your new balance is ${self.balance}')
+  
+  def withdraw(self, amount):
+    if amount < 0:
+      print('You cannot withdraw or deposit negative amounts.')
+      return False
+    elif self.balance - amount < 0:
+      print(f'You do not have enough in your balance (currently ${self.balance} to withdraw ${amount}')
+      return False
+    else:
+      self.balance -= amount
+      print(f'After withdrawing ${amount}, your new balance is ${self.balance}')
 
-class OverdraftAccount:
-  pass
+  def accumulate_interest(self):
+    self.balance *= (1 + self.interest_rate)
+    print(f'Your new balance is ${self.balance}')
+
+class ChildrensAccount(BankAccount):
+  def __init__(self, interest_rate=0):
+    super().__init__(interest_rate)
+  
+  def accumulate_interest(self):
+    self.balance += 10
+    print(f'Your new balance is ${self.balance}')
+
+class OverdraftAccount(BankAccount):
+  def __init__(self, overdraft_penalty=40):
+    super().__init__()
+    self.overdraft_penalty = overdraft_penalty
+
+  def withdraw(self, amount):
+    if self.balance - amount < 0:
+      self.balance -= self.overdraft_penalty
+      print(f'You do not have enough in your balance to withdraw ${amount}. You have been charged an overdraft penalty of ${self.overdraft_penalty}, which has been deducted from your account. Your balance is now ${self.balance}')
+      return False
+    else: 
+      self.balance -= amount
+      print(f'After withdrawing ${amount}, your new balance is ${self.balance}')
+  
+  def accumulate_interest(self):
+    if (self.balance < 0):
+      return
 
 basic_account = BankAccount()
 basic_account.deposit(600)
