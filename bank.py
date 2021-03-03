2aee15adc0f6dc910bbb9afb8a0e2638e3858a99
kys3923 <kys3923@gmail.com>
2021-03-02 20:03:39 -0500

    started hw, stubbed out pseudo codes

--- bank.py
+++ bank.py
@@ -1,10 +1,27 @@
 class BankAccount:
-  pass
+  def __init__(self, amount):
+    # balance
+    self.balance = 0
+    self.transaction = amount
+    # deposit - returns the balance of the account after adding the deposited amount
+    def deposit(self):
+      self.balance += self.transaction
+    # withdraw - returns the balance after money was successfully withdrawn
+    # Account returns false if someone withdraw more than balance
+    # default intereste rate of 2%
+    # has accumulate interested method, balance = balance + interest rate earned
+    
+    
 
 class ChildrensAccount:
+    # balance
+    # $10 added to the account if accumulate interest is queued (no interest)
   pass
 
 class OverdraftAccount:
+    # has a penalty to user when trying to withdraw more than balance, penalty = $40
+    # if above happends, withdraw is being canceled, and apply penalty to the balance
+    # no interest if the balance is negative number
   pass
 
 basic_account = BankAccount()
