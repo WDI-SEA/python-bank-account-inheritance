a92a85d06ed43054fba5848480f51c5867d14621
ElyssaW <ejwinch1@gmail.com>
2021-03-02 19:30:30 -0600

    Finished

--- bank.py
+++ bank.py
@@ -1,11 +1,79 @@
-class BankAccount:
-  pass
+class BankAccount():
+  # Init
+  def __init__(self, balance=0):
+    # Balance
+    self.balance = balance
+    # Interest rate - default to .02
+    self.interest = .02
 
-class ChildrensAccount:
-  pass
+  # Deposit
+  def deposit(self, amount):
+    # Check if negative
+    if amount <= 0:
+      # Return false
+      return False
+    # Add amount to balance
+    self.balance += amount
+    # Return new balance
+    return self.balance
 
-class OverdraftAccount:
-  pass
+  # Withdraw
+  def withdraw(self, amount):
+    # Check if negative
+    if amount <= 0:
+      # Return false
+      return False
+    # Remove amount from balance
+    self.balance -= amount
+    # Return new balance
+    return self.balance
+
+  # accumulate_interest
+  def accumulate_interest(self):
+    # balance + balance * interest
+    self.balance = self.balance + (self.balance * self.interest)
+    # Return balance
+    return self.balance
+
+# Child account (Inherit from BankAccount)
+class ChildrensAccount(BankAccount):
+  # Init (BankAccount)
+  def __init__(self, balance=0):
+    # set super
+    super().__init__(balance)
+    # set interest rate to 0
+    self.interest = 0
+
+  # accumulate_interest
+  def accumulate_interest(self):
+    # Balance = balance + 10
+    self.balance += 10
+    return self.balance
+
+# Overdraft (Inherit BankAccount)
+class OverdraftAccount(BankAccount):
+  # Init (BankAccount)
+  def __init__(self, balance=0):
+    super().__init__(balance)
+    # Overdraft penalty = 20
+    self.overdraft_penalty = 20
+
+  # Withdraw
+  def withdraw(self, amount):
+    # Checks if the amount is more than in the account
+    if amount > self.balance:
+      self.balance -= self.overdraft_penalty
+      return False
+    else:
+      super().withdraw(amount)
+
+  # accumulate_interest
+  def accumulate_interest(self):
+    # Does not run interest if amount is 0 or less
+    if self.balance <= 0:
+      return False
+    else:
+      super().accumulate_interest()
 
 basic_account = BankAccount()
 basic_account.deposit(600)
