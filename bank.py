c48481e9c60a372e6b5593d7efa58c4e3da04d85
tkadima <kadima.t@husky.neu.edu>
2021-03-04 22:11:37 -0500

    I understand functions better yay

--- bank.py
+++ bank.py
@@ -1,35 +1,47 @@
 class BankAccount():
   def __init__(self, balance=0):
     self.balance = balance
-
-
-  def __str__(self):
-    return 'Your checking is ${}'.format(self.checking)
-  
-  def __str__(self):
-    return 'Your savings is ${}'.format(self.checking)
+    self.interest_rate = .02
+    # self.accumulate_interest = accumulate_interest
 
   def deposit(self, amount):
     self.balance += amount
 
   def withdraw(self, amount):
-    self.balance -= amount
+    if amount > 0:
+      self.balance -= amount
+    elif amount < 0:
+      return False
+    elif amount > balance:
+       self.balance -= overdraft_penalty  
+      
+  def accumulate_interest(self):
+    self.balance = self.balance + (self.balance * self.interest_rate)
+   
 
 
-class ChildrensAccount():
-  super().__init__(savings, checking, amount)
+class ChildrensAccount(BankAccount):
+  def __init__(self, balance=0):
+    super().__init__()
+    self.balance = balance
+    self.interest_rate = 0
+   
+  
  
 
-class OverdraftAccount():
-  def withdraw(self, amount):
+class OverdraftAccount(BankAccount):
+  def __init__(self, overdraft_penalty=40):
+    super().__init__()
+   
 
+    
   
 
 
 basic_account = BankAccount()
 basic_account.deposit(600)
 print("Basic account has ${}".format(basic_account.balance))
-basic_account.withdraw(17)
+basic_account.withdraw(20)
 print("Basic account has ${}".format(basic_account.balance))
 basic_account.accumulate_interest()
 print("Basic account has ${}".format(basic_account.balance))
