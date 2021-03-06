7245c3b3b1e505bef8b461cf30aed9c8f36b6fee
edJaPe <edv06jara@gmail.com>
2021-03-06 11:18:04 -0800

    complete and test

--- bank.py
+++ bank.py
@@ -1,3 +1,4 @@
+import math
 class BankAccount:
   # pass
   def __init__(self, balance):
@@ -24,42 +25,68 @@ class BankAccount:
     
 
 
-  # def accumulate_interest(self, interest)
-  #   self.interest = 100 * int(interest)
-  #   self.balance = self.interest * self.balance + self.balance
-  #   return self.balance
+  def accumulate_interest(self, interest):
+    self.interest = interest / 100
+    self.balance = (self.interest * self.balance) + self.balance
+    return math.floor(int(self.balance))
 
+class ChildrensAccount(BankAccount):
+  def __init__(self, balance):
+    super().__init__(balance)
 
+  def accumulate_interest(self, money):
+    self.money = money
+    self.balance = self.balance + 10
+    return self.balance
 
+class OverdraftAccount(BankAccount):
+  def __init__(self, balance):
+    super().__init__(balance)
 
-class ChildrensAccount:
-  pass
+  def withdraw(self, withdraw):
+    self.withdraw = withdraw
+    self.overdraft_penalty = 40
+    if (self.balance - withdraw < 0):
+      self.balance = self.balance - self.overdraft_penalty
+      print("False")
+      return self.balance
+    else:
+      self.balance =self.balance - withdraw
+      return self.balance
 
-class OverdraftAccount:
-  pass
+  def accumulate_interest(self, interest):
+    if self.balance < 0:
+      print("False")
+      return self.balance
+    else:
+      self.interest = interest / 100
+      self.balance = (self.interest * self.balance) + self.balance
+      return math.floor(int(self.balance))
 
-basic_account = BankAccount(500)
-basic_account.deposit(-600)
+    
+
+basic_account = BankAccount(583)
+basic_account.deposit(17)
 print("Basic account has ${}".format(basic_account.balance))
-basic_account.withdraw(-500)
+basic_account.withdraw(-17)
 print("Basic account has ${}".format(basic_account.balance))
-# basic_account.accumulate_interest()
-# print("Basic account has ${}".format(basic_account.balance))
+basic_account.accumulate_interest(2)
+print("Basic account plus interest has ${}".format(basic_account.balance))
 # print()
 
-# childs_account = ChildrensAccount()
-# childs_account.deposit(34)
-# print("Child's account has ${}".format(childs_account.balance))
-# childs_account.withdraw(17)
-# print("Child's account has ${}".format(childs_account.balance))
-# childs_account.accumulate_interest()
-# print("Child's account has ${}".format(childs_account.balance))
+childs_account = ChildrensAccount(0)
+childs_account.deposit(34)
+print("Child's account has ${}".format(childs_account.balance))
+childs_account.withdraw(17)
+print("Child's account has ${}".format(childs_account.balance))
+childs_account.accumulate_interest(58)
+print("Child's account has ${}".format(childs_account.balance))
 # print()
 
-# overdraft_account = OverdraftAccount()
-# overdraft_account.deposit(12)
-# print("Overdraft account has ${}".format(overdraft_account.balance))
-# overdraft_account.withdraw(17)
-# print("Overdraft account has ${}".format(overdraft_account.balance))
-# overdraft_account.accumulate_interest()
-# print("Overdraft account has ${}".format(overdraft_account.balance))
+overdraft_account = OverdraftAccount(0)
+overdraft_account.deposit(12)
+print("Overdraft account has ${}".format(overdraft_account.balance))
+overdraft_account.withdraw(17)
+print("Overdraft account has ${}".format(overdraft_account.balance))
+overdraft_account.accumulate_interest(3)
+print("Overdraft account has ${}".format(overdraft_account.balance))
