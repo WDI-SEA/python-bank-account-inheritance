b2e30000954fc39d93535947d2b890607cc2beda
dinosaur96m <nccadmantinaos@gmail.com>
2021-12-22 23:44:46 -0500

    program returns expected outputs

--- bank.py
+++ bank.py
@@ -1,11 +1,53 @@
+import math
 class BankAccount:
-  pass
+  def __init__(self):
+    self.balance = 0
+    self.interest_rate = 1.02
 
-class ChildrensAccount:
-  pass
+  def deposit(self, amount):
+    if math.fabs(amount) == amount:
+      self.balance += amount
+      return self.balance
+    elif math.fabs(amount) != amount:
+      return False
 
-class OverdraftAccount:
-  pass
+  def withdraw(self, amount):
+    if math.fabs(amount) == amount:
+      self.balance -= amount
+      return self.balance
+    elif math.fabs(amount) != amount:
+      return False
+  
+  def accumulate_interest(self):
+    self.balance *= self.interest_rate
+    return self.balance
+
+class ChildrensAccount(BankAccount):
+  def __init__(self):
+    super().__init__()
+    self.interest_rate = 1.0
+  
+  def accumulate_interest(self):
+    self.balance += 10
+    return self.balance 
+
+class OverdraftAccount(BankAccount):
+  def __init__(self):
+    super().__init__()
+    self.overdraft_penalty = 40
+
+  def withdraw(self, amount): 
+    if self.balance - amount < 0:
+      self.balance -= self.overdraft_penalty
+      return False
+    else:
+      self.balance -= amount
+
+  def accumulate_interest(self):
+    if self.balance < 0:
+      return False
+    else: 
+      self.balance *= self.interest_rate
 
 basic_account = BankAccount()
 basic_account.deposit(600)
