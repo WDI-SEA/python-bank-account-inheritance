53b4c99c20075d0e5cc0d000cd13e2dba27c0b14
weston-bailey <64712575+weston-bailey@users.noreply.github.com>
2022-07-06 10:01:28 -0700

    Update bank.py

--- bank.py
+++ bank.py
@@ -19,26 +19,26 @@ try:
 except Exception as e:
   print(e)
 
-try:
-  childs_account = ChildrensAccount()
-  childs_account.deposit(34)
-  print("Child's account has ${}".format(childs_account.balance))
-  childs_account.withdraw(17)
-  print("Child's account has ${}".format(childs_account.balance))
-  childs_account.accumulate_interest()
-  print("Child's account has ${}".format(childs_account.balance))
-  print()
-except Exception as e:
-  print(e)
+# try:
+#   childs_account = ChildrensAccount()
+#   childs_account.deposit(34)
+#   print("Child's account has ${}".format(childs_account.balance))
+#   childs_account.withdraw(17)
+#   print("Child's account has ${}".format(childs_account.balance))
+#   childs_account.accumulate_interest()
+#   print("Child's account has ${}".format(childs_account.balance))
+#   print()
+# except Exception as e:
+#   print(e)
   
 
-try:
-  overdraft_account = OverdraftAccount()
-  overdraft_account.deposit(12)
-  print("Overdraft account has ${}".format(overdraft_account.balance))
-  overdraft_account.withdraw(17)
-  print("Overdraft account has ${}".format(overdraft_account.balance))
-  overdraft_account.accumulate_interest()
-  print("Overdraft account has ${}".format(overdraft_account.balance))
-except Exception as e:
-  print(e)
+# try:
+#   overdraft_account = OverdraftAccount()
+#   overdraft_account.deposit(12)
+#   print("Overdraft account has ${}".format(overdraft_account.balance))
+#   overdraft_account.withdraw(17)
+#   print("Overdraft account has ${}".format(overdraft_account.balance))
+#   overdraft_account.accumulate_interest()
+#   print("Overdraft account has ${}".format(overdraft_account.balance))
+# except Exception as e:
+#   print(e)
